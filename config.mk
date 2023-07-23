eba63a00658d6ac78ed3e2dd742bc6e9d3443cf1
alpheratz0 <alpheratz99@protonmail.com>
2023-07-23 03:22:30 -0300

    format config.mk

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 # Copyright (C) 2022 <alpheratz99@protonmail.com>
 # This program is free software.
 
-PREFIX    = /usr/local
-MANPREFIX = $(PREFIX)/share/man
+PREFIX=/usr/local
+MANPREFIX=$(PREFIX)/share/man
